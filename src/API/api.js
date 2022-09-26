49a1042a687700e9256e60c317cda31da04cb2d4
LAPTOP-1UIQE60B\OperThirteen <oprthrtn@gmail.com>
2022-09-26 19:03:19 +0700

    Реализованы API-запросы /Task_#12901
    Установлен npm-пакет Axios

---
+++ src/API/api.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const apiUrl = 'http://yummycookingbackend/';
+
+async function GetRecipes(){
+    return await axios.get(apiUrl + 'getRecipes').then((response) => {
+        return response.data;
+    });
+}
+
+
+async function Auth(username, password){
+    return await axios.post(apiUrl + 'auth', {username: username, password: password}).then((response) => {
+        return response;
+    });
+}
+
+async function Register(username, password){
+    return await axios.post(apiUrl + 'register', {username: username, password: password}).then((response) => {
+        return response;
+    });
+}
+
+async function Logout(){
+    return await axios.post(apiUrl + 'logout').then((response) => {
+        return response;
+    });
+}
+
+async function AddToFavorites(recipeId){
+    return await axios.post(apiUrl + 'addToFavorites', {recipeId: recipeId}).then((response) => {
+        return response;
+    });
+}
+
+async function DeleteRecipe(recipeId){
+    return await axios.delete(apiUrl + `deleteRecipe/${recipeId}`).then((response) => {
+        return response;
+    });
+}
+
+
+async function PostRecipe(body){
+    return await axios.post(apiUrl + 'postRecipe', {body}).then((response) => {
+        return response;
+    });
+}
+
+export const api = {
+    GetRecipes : GetRecipes,
+    Auth : Auth,
+    Register : Register,
+    Logout : Logout,
+    AddToFavorites : AddToFavorites,
+    DeleteRecipe : DeleteRecipe,
+    PostRecipe : PostRecipe
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import { api } from './API/api';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    async function getRecipes() {
+      const recipes = await api.GetRecipes();
+      console.log(recipes);
+    }
+
+    getRecipes();
+  }, [])
+
   return (
     <div className="App">
     </div>
