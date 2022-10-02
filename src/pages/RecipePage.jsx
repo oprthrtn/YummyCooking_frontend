ef09dfe6273c65480941bea54702279f43545671
LAPTOP-1UIQE60B\OperThirteen <oprthrtn@gmail.com>
2022-10-02 18:57:54 +0700

    Свёрстана страница кулинарного рецепта /Task_#12892

---
+++ src/pages/RecipePage.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+function RecipePage() {
+    return (
+        <Card>
+            <Card.Body>
+                RecipePage
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default RecipePage;
