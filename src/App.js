d9e8518dbbc05bc95cae27138385b3cb32c0d84b
LAPTOP-1UIQE60B\OperThirteen <oprthrtn@gmail.com>
2022-10-12 17:05:06 +0700

    Свёрстана главная страница /Task_#12889

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
-import AuthPage from './pages/authPage';
-import CreateRecipePage from './pages/CreateRecipePage';
+import MainPage from './pages/MainPage';
+import CreateRicpePage from './pages/CreateRecipePage';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <AuthPage/>
-      <CreateRecipePage/>
+      <BrowserRouter>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="home">YUMMYCOOKING</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="w-100">
+                <Nav.Link as={Link} to="home">Главная</Nav.Link>
+                <Nav.Link as={Link} to="favorites">Избранное</Nav.Link>
+                <Nav.Link as={Link} to="createRecipe">Создать рецепт</Nav.Link>
+                <Nav.Link as={Link} to="auth" className='mr-auto'>Войти</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+
+        <Routes>
+          <Route path="/home" element={<MainPage />}>
+          </Route>
+          <Route path="/favorites" element={<MainPage />}>
+          </Route>
+          <Route path="/createRecipe" element={<CreateRicpePage />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
